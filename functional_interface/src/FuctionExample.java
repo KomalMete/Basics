3fb449136e1929ba736bb8b0f9553c176c46b067
KomalMete <komalmete8@gmail.com>
2024-06-26 16:07:32 +0530

    Function example 2

--- functional_interface/src/FuctionExample.java
+++ functional_interface/src/FuctionExample.java
@@ -4,8 +4,12 @@ public class FuctionExample {
 
     public static void main(String [] args)
     {
-        Function<Integer, Integer> f = i -> i*i;
+        //square
+//        Function<Integer, Integer> f = i -> i*i;
+//        System.out.println(f.apply(5));
 
-        System.out.println(f.apply(5));
+        //return length of string
+        Function<String, Integer> f = s -> s.length();
+        System.out.println(f.apply("Komal"));
     }
 }
