021c56c8d3b4485baf2070df4cc4f2d49799cec8
KomalMete <komalmete8@gmail.com>
2024-06-26 14:39:10 +0530

    predicate example 2

--- functional_interface/src/Predicate.java
+++ functional_interface/src/Predicate.java
@@ -4,9 +4,14 @@ class PredicateExample
 {
     public static void main(String[] args)
     {
-        Predicate<Integer> p = n -> n>=100 ;
+//        Predicate<Integer> p = n -> n>=100 ;
+//
+//        System.out.println(p.test(10));
+//        System.out.println(p.test(100));
+
+        Predicate<Integer> p = n -> (n%2 == 0);
 
         System.out.println(p.test(10));
-        System.out.println(p.test(100));
+        System.out.println(p.test(45));
     }
 }
