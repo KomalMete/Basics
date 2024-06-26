6fd289492aabdd8732e88711fbe1a06a237d75d8
KomalMete <komalmete8@gmail.com>
2024-06-26 14:54:22 +0530

    predicate example 2

--- functional_interface/src/Predicate.java
+++ functional_interface/src/Predicate.java
@@ -5,13 +5,23 @@ class PredicateExample
     public static void main(String[] args)
     {
 //        Predicate<Integer> p = n -> n>=100 ;
-//
-//        System.out.println(p.test(10));
-//        System.out.println(p.test(100));
+//        System.out.println(p.test(10));// false
+//        System.out.println(p.test(100));//true
 
-        Predicate<Integer> p = n -> (n%2 == 0);
+//        Predicate<Integer> p = n -> (n%2 == 0);
+//        System.out.println(p.test(10));//true
+//        System.out.println(p.test(45));//false
 
-        System.out.println(p.test(10));
-        System.out.println(p.test(45));
+        String[] s = {"Komal", "Virat", "Kiran", "Rohit"};
+
+        Predicate<String> p = n -> (n.charAt(0) == 'K' || n.charAt(0) =='R');
+
+        for(String s1 : s)
+        {
+            if(p.test(s1))
+            {
+                System.out.println(s1); //Komal Kiran Rohit
+            }
+        }
     }
 }
