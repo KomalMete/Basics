78d11107be26e2d9de3a3e4876ead3c2874ae37c
KomalMete <komalmete8@gmail.com>
2024-06-27 17:28:25 +0530

    BiFunction examples started

--- functional_interface/src/BiFunctionExample.java
+++ functional_interface/src/BiFunctionExample.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.*;
 
 class Employee
@@ -17,7 +19,21 @@ public class BiFunctionExample {
     public static void main(String[] args)
     {
         BiFunction<Integer, String, Employee> bf = (eno,name) ->  new Employee(eno, name);
-        Employee emp = bf.apply(1,"Komal");
-        System.out.println(emp.name);
+        //for individual employee
+        //Employee emp = bf.apply(1,"Komal");
+        //System.out.println(emp.name);
+
+        List<Employee> list = new ArrayList<>();
+        list.add(bf.apply(2,"Kritika"));
+        list.add(bf.apply(3,"Katrina"));
+        list.add(bf.apply(4,"Preeti"));
+        list.add(bf.apply(5,"Kajol"));
+
+        for(Employee emp : list)
+        {
+            System.out.println("Employee ID " +emp.employeeNo);
+            System.out.println("Employee name " +emp.name);
+            System.out.println();
+        }
     }
 }
